301cbb4216992f7511789fc142787c1e97cda129
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2024-02-02 17:59:29 +0100

    Add list of remotes and check whether ahead or behind

--- src/git_utils.rs
+++ src/git_utils.rs
@@ -1,5 +1,5 @@
-use git2::{Remote};
 use crate::Error;
+use git2::Remote;
 
 fn get_remote_callbacks<'a>() -> Result<git2::RemoteCallbacks<'a>, Error> {
     let mut callbacks = git2::RemoteCallbacks::new();
@@ -10,7 +10,10 @@ fn get_remote_callbacks<'a>() -> Result<git2::RemoteCallbacks<'a>, Error> {
     Ok(callbacks)
 }
 
-pub(crate) fn get_all_remotes(repo: &git2::Repository, connect: bool) -> Result<Vec<Remote>, Error> {
+pub(crate) fn get_all_remotes(
+    repo: &git2::Repository,
+    connect: bool,
+) -> Result<Vec<Remote>, Error> {
     let mut result = vec![];
     for remote_str in repo.remotes()?.iter() {
         let remote_str = remote_str.ok_or("Remote name is not a valid UTF-8 string")?;
@@ -23,13 +26,60 @@ pub(crate) fn get_all_remotes(repo: &git2::Repository, connect: bool) -> Result<
     Ok(result)
 }
 
-pub(crate) fn is_remote_up_to_date(repo: &git2::Repository, mut remote: Remote) -> Result<bool, Error> {
+#[derive(Debug)]
+pub(crate) enum RemoteStatus {
+    LocalAhead,
+    LocalBehind,
+    UpToDate,
+}
+
+impl RemoteStatus {
+    pub(crate) fn is_clean(&self) -> bool {
+        match self {
+            RemoteStatus::UpToDate => true,
+            _ => false,
+        }
+    }
+}
+impl std::fmt::Display for RemoteStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RemoteStatus::LocalAhead => write!(f, "ahead of remote"),
+            RemoteStatus::LocalBehind => write!(f, "behind remote"),
+            RemoteStatus::UpToDate => write!(f, ""),
+        }
+    }
+}
+
+pub(crate) fn is_remote_up_to_date(
+    repo: &git2::Repository,
+    mut remote: Remote,
+) -> Result<RemoteStatus, Error> {
     remote.update_tips(None, false, git2::AutotagOption::Unspecified, None)?;
-    let remote_head = remote.list()?.iter().filter(|x| x.name() == "HEAD").next().ok_or("Remote HEAD not found")?.oid();
+    let remote_head = remote
+        .list()?
+        .iter()
+        .filter(|x| x.name() == "HEAD")
+        .next()
+        .ok_or("Remote HEAD not found")?
+        .oid();
 
     let default_branch = remote.default_branch()?;
     let default_branch = default_branch.as_str().unwrap();
 
     let local_head = repo.refname_to_id(default_branch)?;
-    Ok(local_head == remote_head)
+
+    if local_head == remote_head {
+        return Ok(RemoteStatus::UpToDate);
+    }
+
+    let local_ahead = repo.graph_descendant_of(local_head, remote_head)?;
+    let local_behind = repo.graph_descendant_of(remote_head, local_head)?;
+
+    match (local_ahead, local_behind) {
+        (true, false) => Ok(RemoteStatus::LocalAhead),
+        (false, true) => Ok(RemoteStatus::LocalBehind),
+        (true, true) => Err("Local commit is both ahead and behind remote!".into()),
+        (false, false) => Err("Local commit is neither ahead nor behind remote!".into()),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod git_utils;
 
-use std::fmt::Display;
-use std::path::{Path, PathBuf};
-use structopt::StructOpt;
-use trash;
-use std::process::{Command, Stdio};
+use colored::Colorize;
 use git2::{Repository, Status};
 use multipeek::multipeek;
-use colored::Colorize;
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::fmt::Display;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+use structopt::StructOpt;
+use trash;
 
 type Error = Box<dyn std::error::Error>;
 
@@ -17,16 +17,18 @@ type Error = Box<dyn std::error::Error>;
 #[derive(StructOpt)]
 #[structopt(name = "lazy-git-clean")]
 struct Opt {
-
     /// Repo folders to process
     #[structopt(name = "REPOS", parse(from_os_str))]
-    repos: Vec<PathBuf>
+    repos: Vec<PathBuf>,
 }
 
 #[allow(dead_code)] // For future use
 fn inquire_select<'a, T>(prompt: &str, options: &'a Vec<(&str, T)>) -> &'a T {
     let options_str = options.iter().map(|x| x.0).collect::<Vec<_>>();
-    let selected = inquire::Select::new(prompt,options_str).prompt().unwrap().to_string();
+    let selected = inquire::Select::new(prompt, options_str)
+        .prompt()
+        .unwrap()
+        .to_string();
     let selected = options.iter().filter(|x| x.0 == selected).next().unwrap();
     &selected.1
 }
@@ -39,7 +41,11 @@ fn bool_to_checkmark(b: bool) -> &'static str {
     }
 }
 
-fn print_subsection<Item: Display, Container: IntoIterator<Item=Item>> (items: Container, limit: usize, indent: usize) {
+fn print_subsection<Item: Display, Container: IntoIterator<Item = Item>>(
+    items: Container,
+    limit: usize,
+    indent: usize,
+) {
     let mut items = multipeek(items.into_iter());
     let mut count = 0;
     while let Some(item) = items.next() {
@@ -54,11 +60,10 @@ fn print_subsection<Item: Display, Container: IntoIterator<Item=Item>> (items: C
     }
 }
 
-struct RepoStatus{
+struct RepoStatus {
     remotes_clean: bool,
-    files_clean: bool
+    files_clean: bool,
 }
-
 
 impl RepoStatus {
     fn is_clean(&self) -> bool {
@@ -72,46 +77,86 @@ impl RepoStatus {
             "not clean"
         }
     }
-
 }
 fn get_repo_status_verbose(repo: &Repository) -> Result<RepoStatus, Error> {
     let remotes = git_utils::get_all_remotes(&repo, true)?;
-    let remotes_status : Vec<Result<bool, Error>> = remotes.into_iter().map(|x| git_utils::is_remote_up_to_date(&repo, x)).collect();
-    let remotes_clean = remotes_status.iter().all(|x| *x.as_ref().ok().unwrap_or(&false));
+    let remotes: Vec<_> = remotes
+        .into_iter()
+        .map(|x| {
+            let remote_name: String = x.name().unwrap().to_string();
+            let status = git_utils::is_remote_up_to_date(&repo, x);
+            dbg!(&remote_name, &status);
+            (remote_name, status)
+        })
+        .collect();
+    let remotes_clean = remotes
+        .iter()
+        .map(|x| x.1.as_ref().ok().and_then(|x| Some(x.is_clean())))
+        .all(|x| x.unwrap_or(false));
     println!("{} Remotes up to date", bool_to_checkmark(remotes_clean));
+    let remote_msgs = remotes
+        .iter()
+        .map(|(remote_name, status)| {
+            let status_bool = status.as_ref().map(|x| x.is_clean()).unwrap_or(false);
+            let status_msg = match status {
+                Ok(x) => x.to_string(),
+                Err(e) => e.to_string(),
+            };
+            format!(
+                "{} {}: {}",
+                bool_to_checkmark(status_bool),
+                remote_name,
+                status_msg
+            )
+        })
+        .collect::<Vec<_>>();
+    print_subsection(remote_msgs, 5, 4);
 
-    let unsafe_to_delete = |status : Status| {
-        !status.is_ignored() &&
-            (status.is_wt_new() || status.is_wt_modified() || status.is_index_new() || status.is_index_modified() || status.is_conflicted())
+    let unsafe_to_delete = |status: Status| {
+        !status.is_ignored()
+            && (status.is_wt_new()
+                || status.is_wt_modified()
+                || status.is_index_new()
+                || status.is_index_modified()
+                || status.is_conflicted())
     };
 
     let statuses = repo.statuses(None)?;
-    let mut unsafe_files = statuses.iter()
+    let mut unsafe_files = statuses
+        .iter()
         .filter(|x| unsafe_to_delete(x.status()))
         .map(|x| x.path().unwrap().to_string())
         .collect::<Vec<_>>();
     unsafe_files.shuffle(&mut thread_rng());
 
-
-    let mut ignored_files = statuses.iter()
+    let mut ignored_files = statuses
+        .iter()
         .filter(|x| x.status().is_ignored())
         .map(|x| x.path().unwrap().to_string())
         .collect::<Vec<_>>();
     ignored_files.shuffle(&mut thread_rng());
 
-    println!("{} Not ignored files clean", bool_to_checkmark(unsafe_files.is_empty()));
+    println!(
+        "{} Not ignored files clean",
+        bool_to_checkmark(unsafe_files.is_empty())
+    );
     print_subsection(unsafe_files.iter(), 5, 4);
     println!("✅ The following ignored files will be deleted:");
     print_subsection(ignored_files, 5, 4);
 
-    Ok(RepoStatus{
+    Ok(RepoStatus {
         remotes_clean,
-        files_clean: unsafe_files.is_empty()
+        files_clean: unsafe_files.is_empty(),
     })
 }
 
 fn which(bin: &str) -> bool {
-    Command::new("which").arg(bin).stdout(Stdio::null()).status().unwrap().success()
+    Command::new("which")
+        .arg(bin)
+        .stdout(Stdio::null())
+        .status()
+        .unwrap()
+        .success()
 }
 
 fn process_repo(path: &Path) -> Result<bool, Error> {
@@ -123,7 +168,7 @@ fn process_repo(path: &Path) -> Result<bool, Error> {
 
     while changed {
         changed = false;
-        if !first_run{
+        if !first_run {
             status = get_repo_status_verbose(&repo)?;
         }
         first_run = false;
@@ -132,7 +177,10 @@ fn process_repo(path: &Path) -> Result<bool, Error> {
         }
         if !used_lazygit && which("lazygit") {
             used_lazygit = true;
-            if !inquire::Confirm::new("Do you want to use lazygit?").prompt().unwrap() {
+            if !inquire::Confirm::new("Do you want to use lazygit?")
+                .prompt()
+                .unwrap()
+            {
                 continue;
             }
             let mut cmd = Command::new("lazygit");
@@ -143,12 +191,20 @@ fn process_repo(path: &Path) -> Result<bool, Error> {
         }
     }
 
-    let answer_first = inquire::Confirm::new(&format!("Repo is {}. Do you want to delete it?", status.is_clean_str())).prompt().unwrap();
-    if !answer_first{
+    let answer_first = inquire::Confirm::new(&format!(
+        "Repo is {}. Do you want to delete it?",
+        status.is_clean_str()
+    ))
+    .prompt()
+    .unwrap();
+    if !answer_first {
         return Ok(false);
     }
     if !status.is_clean() {
-        let answer_second = inquire::Confirm::new("Repo is not clean. Do you want to delete it anyway?").prompt().unwrap();
+        let answer_second =
+            inquire::Confirm::new("Repo is not clean. Do you want to delete it anyway?")
+                .prompt()
+                .unwrap();
         if !answer_second {
             return Ok(false);
         }
@@ -160,7 +216,7 @@ fn main() {
     let opt = Opt::from_args();
     for repo in opt.repos {
         let path = repo.as_path();
-        println!("Processing {path}", path=path.to_str().unwrap());
+        println!("Processing {path}", path = path.to_str().unwrap());
         match process_repo(path) {
             Ok(true) => {
                 trash::delete(path).unwrap();
@@ -168,7 +224,10 @@ fn main() {
             Ok(false) => {}
             Err(e) => {
                 eprintln!("{}", e.to_string().red());
-                if inquire::Confirm::new("Do you want to delete repository anyway?").prompt().unwrap() {
+                if inquire::Confirm::new("Do you want to delete repository anyway?")
+                    .prompt()
+                    .unwrap()
+                {
                     if inquire::Confirm::new("Are you sure?").prompt().unwrap() {
                         trash::delete(path).unwrap();
                     }
